9d36dbd645ce581315d17d0388189f2ce077ca61
AshFlame <bushmeleva.anastasia@gmail.com>
2015-05-19 19:08:46 +0500

    add the second test - testallones()

--- BowlinGameKata/UnitTestGame/UnitTest1.cs
+++ BowlinGameKata/UnitTestGame/UnitTest1.cs
@@ -20,5 +20,18 @@ namespace UnitTestGame
             Assert.AreEqual(0, g.Score());
         }
 
+        [TestMethod]
+        public void TestAllGame()
+        {
+            Game g = new Game();
+
+            for (int i = 0; i < 20; i++)
+            {
+                g.Roll(1);
+            }
+
+            Assert.AreEqual(20, g.Score());
+        }
+
     }
 }
